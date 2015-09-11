daa9ff58633d90afaf094320f53f937b4f4a70be
Rolf Madsen <rolfrm@gmail.com>
2015-09-11 15:31:29 +0200

    Working on improving glsl compile errors

--- gl-ext.lisp
+++ gl-ext.lisp
@@ -49,17 +49,50 @@
 ;;     (gl:gen-buffers 2 (addrof (member msh vbo))) ;vbo and ibo
 ;;     (gl:bind-buffer gl:array-buffer (member msh vbo))
 ;;     (gl:buffer-data gl:array-buffer (cast (member _mesh vertexes) (ptr void)))))
+
+(type (alias u32 gl:error))
+
+(defstruct gl:error-info
+  (id gl:error)
+  (error-info (ptr expr)))
+
+(defvar gl:shader-program-error (cast 2 gl:error))
+(defvar gl:shader-error (cast 1 gl:error))
+(defvar gl:no-error (cast 0 gl:error))
+
+(defvar gl:error-list (cast null (ptr gl:error-info)))
+(defvar gl:error-list-cnt 0)
+(defmacro gl:deferror (name value error-info)
+  (let((error-instance :type gl:error-info))
+    (setf (member error-instance id) (cast (expr2number value) gl:error))
+    (setf (member error-instance error-info) error-info)
+    (add-to-list+ gl:error-list gl:error-list-cnt error-instance)
+    (expr 
+     (defvar (unexpr name) (cast (unexpr value) gl:error)))))
+
+
+(defun gl:get-error-message ((ptr expr) (id gl:error))
+  (let ((o :type gl:error-info))
+    (range it 0 gl:error-list-cnt
+	   (let ((ins (deref (+ gl:error-list it))))
+	     (when (eq (member ins id) id)
+	       (setf o ins)
+	       (setf it gl:error-list-cnt))))
+    (member o error-info)))
+     
     
-  
-    
-(defun gl-ext:print-shader-errors (void (shader gl:shader))
+
+(defun gl-ext:print-shader-errors (gl:error (shader gl:shader))
   (let ((glstatus (cast 0 u32))
 	(log (cast (alloc0 1000) (ptr char))))
     (gl:get-shader-info shader gl:compile-status (addrof glstatus))
-    (when (eq glstatus gl:false)
-      (let ((length :type u32))
-	(gl:get-shader-info-log shader 1000 (addrof length) log)
-	(print "**** Shader Info Log ****" newline log newline "********" newline)))))
+    (if (eq glstatus gl:false)
+	(progn
+	  (let ((length :type u32))
+	    (gl:get-shader-info-log shader 1000 (addrof length) log)
+	    (print "**** Shader Info Log ****" newline log newline "********" newline))
+	  gl:shader-error)
+	gl:no-error)))
 
 (defun load-shader (gl:shader-program (frag-src (ptr char)) (vert-src (ptr char)))
   (let ((prog (gl:create-program))
@@ -71,10 +104,11 @@
     (gl:shader-source vert 1 (addrof vert-src) (addrof vert-src-len))
     (gl:compile-shader frag)
     (print "**** Fragment Shader ****" newline)
-    (gl-ext:print-shader-errors frag)
+    (assert (eq (gl-ext:print-shader-errors frag) gl:no-error))
+      
     (gl:compile-shader vert)
     (print "**** Vertex Shader ****" newline)
-    (gl-ext:print-shader-errors vert)
+    (assert (eq (gl-ext:print-shader-errors vert) gl:no-error))
     (gl:attach-shader prog frag)
     (gl:attach-shader prog vert)
     (gl:link-program prog)    
